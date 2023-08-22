e579a43676edc13c33383126d6889565f875f3b1
sadat <shadowak47@yahoo.com>
2023-08-22 21:42:00 +0100

    Styled AddCycleDetailsForm and worked on configuring firestore, still work in progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-// import React, { useState } from "react";
-// import "./App.css";
-// import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-// // import { signOut } from "firebase/auth";
-// // import firebase from './firebase'
-// // eslint-disable-next-line
-// import AuthDetails from "./components/auth/AuthDetails";
-// import Register from "./pages/RegisterPage";
-// import Signin from "./pages/SigninPage";
-// import FirebaseAuthService from "./FirebaseAuthService";
-
 import React, { useState } from "react";
-import Signin from './components/auth/Signin'
+import Signin from "./components/auth/Signin";
+import AddCycleDetailsForm from "./components/general/AddCycleDetailsForm";
 import FirebaseAuthService from "./FirebaseAuthService";
+import FirebaseFirestoreService from "./FirebaseFirestoreService";
 
 const App = () => {
   const [user, setUser] = useState(null);
 
-  FirebaseAuthService.checkAuthChanges(setUser)
-  return <div>
-    <h1>CycleSync</h1>
-    <Signin existingUser={user}/>
-  </div>;
+  FirebaseAuthService.checkAuthChanges(setUser);
+
+  const handleAddCycleDetails = async (newDetails) => {
+    try {
+      const response = await FirebaseFirestoreService.createDocument('cycle_details', newDetails);
+
+      // TODO: fetch new cycle details from firestore
+
+      alert(`Successfully added cycle details with ID = ${response.id}`)
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+  return (
+    <>
+      <div>
+        <h1>CycleSync</h1>
+        <Signin existingUser={user} />
+      </div>
+      <div>
+        <AddCycleDetailsForm handleAddCycleDetails={handleAddCycleDetails}/>
+      </div>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/general/AddCycleDetailsForm.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import "../../styles/AddCycleDetailsForm.css";
+
+const AddCycleDetailsForm = ({ handleAddCycleDetails }) => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [cycleStartDate, setCycleStartDate] = useState("");
+  const [cycleEndDate, setCycleEndDate] = useState("");
+
+  const handleSubmitForm = (e) => {
+    e.preventDefult();
+
+    const newCycleDetails = {
+      name,
+      age,
+      cycleStartDate,
+      cycleEndDate,
+    };
+
+    handleAddCycleDetails(newCycleDetails)
+  };
+
+  const pageModel = (
+    <div className="container-lg">
+      <form onSubmit={handleSubmitForm} className="mb-5 detailForm">
+        <h3 className="mt-5">Details</h3>
+        <label htmlFor="floatingInput">
+          Name:
+          <input
+            type="text"
+            className="form-control mt-2"
+            id="floatingInput"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label htmlFor="floatingAge">
+          Age:
+          <input
+            type="number"
+            value={age}
+            className="form-control mt-2"
+            id="floatAge"
+            onChange={(e) => setAge(e.target.value)}
+          />
+        </label>
+
+        <h5 className="mt-5">Cycle Information</h5>
+        <label htmlFor="floatCycleStartDate">
+          Start date:
+          <input
+            type="date"
+            value={cycleStartDate}
+            onChange={(e) => setCycleStartDate(e.target.value)}
+            className="form-control mt-2"
+            id="floatCycleStartDate"
+            required
+          />
+        </label>
+        <label htmlFor="floatCycleEndDate">
+          End date:
+          <input
+            type="date"
+            value={cycleEndDate}
+            onChange={(e) => setCycleEndDate(e.target.value)}
+            className="form-control mt-2"
+            id="floatCycleEndDate"
+            required
+          />
+        </label>
+        <button type="submit" className="btn btn-primary mt-3">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+
+  return pageModel;
+};
+
+export default AddCycleDetailsForm;
