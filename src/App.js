543f7c733ccad3bb6d8e0f172a66f1dc14a12a44
sadat <shadowak47@yahoo.com>
2023-08-24 15:36:06 +0100

    Removed unused vars from App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Register from "./components/auth/Register";
 // eslint-disable-next-line
 import Signin from "./components/auth/Signin";
