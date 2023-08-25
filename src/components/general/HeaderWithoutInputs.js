64aa16b7904f5e94cca8264976f873b6ac74b335
sadat <shadowak47@yahoo.com>
2023-08-25 20:44:10 +0100

    Styled links in header without inputs

--- src/components/general/HeaderWithoutInputs.js
+++ src/components/general/HeaderWithoutInputs.js
@@ -22,17 +22,29 @@ const HeaderWithoutInputs = () => {
           <div className="collapse navbar-collapse" id="navbarText">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item custom-nav-item">
-                <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+                <Link to="/"  style={{
+                  textDecoration: "none",
+                  color: "black",
+                  fontWeight: "300",
+                }}>
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link to="/register" style={{ textDecoration: "none", color: "black" }}>
+                <Link to="/register"  style={{
+                  textDecoration: "none",
+                  color: "black",
+                  fontWeight: "300",
+                }}>
                   Register
                 </Link>
               </li>
               <li className="nav-item">
-                <Link to="/contact" style={{ textDecoration: "none", color: "black" }}>
+                <Link to="/contact"  style={{
+                  textDecoration: "none",
+                  color: "black",
+                  fontWeight: "300",
+                }}>
                   Contact
                 </Link>
               </li>
