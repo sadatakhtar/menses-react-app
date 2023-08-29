889c0c00c337189f27bdc0714f6c9be8b87381ee
sadat <shadowak47@yahoo.com>
2023-08-29 17:17:14 +0100

    Added readDocument function to Firestore service file

--- src/FirebaseFirestoreService.js
+++ src/FirebaseFirestoreService.js
@@ -1,13 +1,18 @@
-import firebase from './FirebaseConfig';
+import firebase from "./FirebaseConfig";
 
 const firestore = firebase.firestore();
 
 const createDocument = (collection, document) => {
-    return firestore.collection(collection).add(document);
-}
+  return firestore.collection(collection).add(document);
+};
+
+const readDocuments = (collection) => {
+  return firestore.collection(collection);
+};
 
 const FirebaseFirestoreService = {
-    createDocument,
-}
+  createDocument,
+  readDocuments,
+};
 
 export default FirebaseFirestoreService;
