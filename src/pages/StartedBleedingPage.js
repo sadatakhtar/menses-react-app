19b80cb6263b9445595e7750278ac27613cfe151
sadat <shadowak47@yahoo.com>
2023-09-14 17:51:36 +0100

    Working on strated bleeding page , wip

--- src/pages/StartedBleedingPage.js
+++ src/pages/StartedBleedingPage.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "../styles/StartedBleedingPage.css";
+import { useSelector } from "react-redux";
 import HeaderWithoutInputs from "../components/general/HeaderWithoutInputs";
+import {
+  getEstablishedCycleStartDate,
+  getEstablishedCycleEndDate,
+} from "../features/usersJourney/UserSlice";
 import Footer from "../components/general/Footer";
 
 const StartedBleedingPage = () => {
+  const lastCycleStartDateRedux = useSelector(getEstablishedCycleStartDate);
+  const presumedLastCycleEndDateRedux = useSelector(getEstablishedCycleEndDate);
   return (
     <>
       <HeaderWithoutInputs />
@@ -12,24 +19,30 @@ const StartedBleedingPage = () => {
           <h3>Last cycle information</h3>
           <div className="label-div">
             <p>start date: </p>
-            <span>fetch date from db/redux</span>
+            <span style={{ color: "green" }}>{lastCycleStartDateRedux}</span>
           </div>
 
           <div className="label-div">
             <p>presumed end date: </p>
-            <span>fetch date from db/redux</span>
+            <span style={{ color: "orange" }}>
+              {presumedLastCycleEndDateRedux}
+            </span>
           </div>
         </div>
         <div className="additional-details">
           <h3>Enter additional details</h3>
           <div className="label-div">
             <p>New bleed start date: </p>
-            <span>fetch date from db/redux</span>
+            <label>
+              <input type="date" required/>
+            </label>
           </div>
 
           <div className="label-div">
             <p>New bleed end date: </p>
-            <span>fetch date from db/redux</span>
+            <label>
+              <input type="date" required/>
+            </label>
           </div>
         </div>
       </div>
